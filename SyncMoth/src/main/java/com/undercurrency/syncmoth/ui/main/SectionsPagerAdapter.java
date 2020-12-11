874654326485f6450abd4477300ca55733afdd8b
Arturo Frappé Muñoz <afrappe@acm.org>
2020-12-11 10:58:08 -0600

    first release issues

--- SyncMoth/src/main/java/com/undercurrency/syncmoth/ui/main/SectionsPagerAdapter.java
+++ SyncMoth/src/main/java/com/undercurrency/syncmoth/ui/main/SectionsPagerAdapter.java
@@ -52,6 +52,6 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         // Show 2 total pages.
-        return 4;
+        return 1;
     }
 }
